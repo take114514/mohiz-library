dc29f74c250efdebadd3949efeb3610b36c5d5bc
MakTak <justaway22@gmail.com>
2019-10-05 17:02:29 +0900

    static pagesのテスト修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Mohiz Library"
   end
 
   test "should get root" do
@@ -14,13 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Mohiz Library"
   end
 
   test "should get about" do
